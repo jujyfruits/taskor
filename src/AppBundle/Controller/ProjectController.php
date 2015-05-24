524176adc9af39c15ad8432af014208b1b7e7cb2
jujyfruits <beavermark93@gmail.com>
2015-05-24 22:30:11 +0400

    added ajax modal

--- src/AppBundle/Controller/ProjectController.php
+++ src/AppBundle/Controller/ProjectController.php
@@ -23,19 +23,22 @@ class ProjectController extends Controller {
         $project_id = $request->get('project_id');
         $task_id = $request->get('task_id');
         $new_state = $request->get('new_state');
-        $estimated_time = $request->get('estimated_time');
-        $spended_time = $request->get('spended_time');
+        $estimated_time = ($new_state == 'deny') ? null : $request->get('estimated_time');
+        $spended_time = ($new_state == 'deny') ? null : $request->get('spended_time');
+
         if (empty($project_id) || empty($task_id) || empty($new_state))
             return;
+
         $em = $this->getDoctrine()->getEntityManager();
         $task = $em->getRepository('AppBundle:Task')->getProjectTaskById($project_id, $task_id);
 
         $project = $em->getRepository('AppBundle:Task')->findOneBy(array('id' => $project_id));
         if (empty($task))
             return;
+
         $task->setEstimatedTime($estimated_time);
         $task->setSpendedTime($spended_time);
-       
+
         if ($new_state == 'start') {
             $task->setState('Started');
             $task->setUser($this->container->get('security.context')->getToken()->getUser());
@@ -49,7 +52,6 @@ class ProjectController extends Controller {
         }
         $em->persist($task);
         $em->flush();
-        $response = new Response();
         $view = $this->renderView('project/tasks/task_list.html.twig', array('task' => $task, 'project' => $project));
         $response = new Response($view);
         return $response;
@@ -59,7 +61,7 @@ class ProjectController extends Controller {
      * @Route("/project/task/ajaxchangestate/dialog",
      *  name="ajax_change_task_state_dialog")
      */
-    public function ajaxChangeTaskStateDialogAction(Request $request) {
+    public function ajaxDialogChangeTaskStateAction(Request $request) {
         if (!$this->getRequest()->isXmlHttpRequest()) {
             throw $this->createNotFoundException();
         }
@@ -74,16 +76,20 @@ class ProjectController extends Controller {
         $form = $this->createFormBuilder($task)
                 ->add('estimated_time', 'integer', array(
                     'label' => 'Предполагаемое время выполнения',
-                    'disabled' => (($new_state == 'finish')||($new_state == 'deny'))
+                    'disabled' => (($new_state == 'finish') || ($new_state == 'deny')),
+                    'data' => ($task->getEstimatedTime()) ? $task->getEstimatedTime() : null,
+                    'attr' => array('min' => 1)
                 ))
                 ->add('spended_time', 'integer', array(
                     'label' => 'Итоговое время выполнения',
-                    'disabled' => (($new_state == 'start')||($new_state == 'deny'))
+                    'disabled' => (($new_state == 'start') || ($new_state == 'deny')),
+                    'data' => ($task->getSpendedTime()) ? $task->getSpendedTime() : null,
+                    'attr' => array('min' => 1)
                 ))
                 ->getForm();
-        $response = new Response();
-        $view = $this->renderView('project/tasks/task_state_dialog.html.twig', array(
+        $view = $this->renderView('project/tasks/task_change_state_dialog.html.twig', array(
             'task' => $task,
+            'new_state' => $new_state,
             'form' => $form->createView()
         ));
         $response = new Response($view);
